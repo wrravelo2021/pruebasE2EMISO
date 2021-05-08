163a68b6153e03747fac55c01d49e67fca32790d
William Ravelo M <drummerwilliam@gmail.com>
2021-05-08 13:11:24 -0500

    second

--- playwright/index.js
+++ playwright/index.js
@@ -54,4 +54,63 @@ const url = 'http://localhost:2368/ghost';
     await browser.close();
   }
   return;
+});
+
+
+(async () => {
+  //Definir los navegadores en los que se quiere hacer la prueba
+  for (const browserType of ['chromium']){//, 'firefox', 'webkit']) {
+    //Contenido de la prueba
+    console.log(browserType+'-------------------------------------------')
+
+    let title = `${Date.now()}`;
+    let body = `${Date.now()} body.`
+
+    //Creación del objeto browser, el contexto del mismo y el objeto page para manejar la página
+    const browser = await playwright[browserType].launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    //Abrir la URL a probar en la página y cargar el proyecto en una SPA
+    await page.goto(url);
+    //await page.screenshot({path: './pagina.png'})
+    await page.type('input[name="identification"]', 'drummerwilliam@gmail.com');
+    await page.type('input[name="password"]', 'pruebasmiso');
+    await page.click('css=button.login');
+    await page.click('.gh-nav-list-new.relative > a[href="#/posts/"]');
+    await page.click('a[href="#/editor/post/"]');
+    await page.type('.gh-editor-title.ember-text-area.gh-input.ember-view', title);
+    await page.click('.koenig-editor__editor.__mobiledoc-editor');
+    page.keyboard.type(body);
+    await page.click('a[href="#/posts/"].blue.link.fw4.flex.items-center.ember-view');
+    await new Promise(r => setTimeout(r, 1000));
+    await page.click('span:has-text("All posts")');
+    await new Promise(r => setTimeout(r, 1000));
+    await page.click('.ember-power-select-option:has-text("Draft posts")');
+    await new Promise(r => setTimeout(r, 1000));
+
+    var postsTitles = await page.$$(".gh-content-entry-title");
+    var text = await postsTitles[0].innerText();
+    assert.equal(text, title);
+
+    await page.click(`.gh-content-entry-title:has-text("${title}")`);
+    await page.click('.gh-btn.gh-btn-outline.gh-publishmenu-trigger.ember-basic-dropdown-trigger.ember-view');
+    await page.click('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view');
+    await page.click('a[href="#/posts/?type=draft"].blue.link.fw4.flex.items-center.ember-view');
+    await page.click('.gh-notification-close');
+    await new Promise(r => setTimeout(r, 1000));
+    await page.click('span:has-text("Draft posts")');
+    await new Promise(r => setTimeout(r, 1000));
+    await page.click('.ember-power-select-option:has-text("Published posts")');
+    await new Promise(r => setTimeout(r, 1000));
+
+    postsTitles = await page.$$(".gh-content-entry-title");
+    text = await postsTitles[0].innerText();
+    assert.equal(text, title);
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    await browser.close();
+  }
+  return;
 })();//Llamado propio de la función
