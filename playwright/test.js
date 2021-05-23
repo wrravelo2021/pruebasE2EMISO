7e3ca80db1be07a51120721c991254cf0c79447c
Gerzon <gerzon.bautista@dafiti.com.co>
2021-05-23 11:37:09 -0500

    Se agrega el escenario F088

--- playwright/test.js
+++ playwright/test.js
@@ -631,6 +631,50 @@ it('F087 - should create a tag with metadata.', async () => {
   assert.strictEqual(existsTag, true);
 });
 
+it('F088 - should not create a new tag when the meta title has more than 300 characteres.', async () => {
+  const nameTag = dataPoolTag.name_tag;
+  const descriptionTag = dataPoolTag.description_tag;
+  const metaTitleTag = faker.datatype.string(301);
+  const metaDescriptionTag = dataPoolTag.meta_description_tag;
+
+  const loginPage = new LoginPage(page);
+  const homePage = new HomePage(page);
+  const tagsPage = new TagsPage(page);
+  const tagDetailPage = new TagDetailPage(page);
+
+  await page.goto(config.url);
+  await loginPage.enterEmail(credentials.email);
+  await loginPage.enterPassword(credentials.password);
+  await loginPage.clickLogin();
+  await homePage.goToTags();
+  await tagsPage.goToCreateNewTag();
+  await tagDetailPage.enterTitleForNewTag(nameTag);
+  await tagDetailPage.enterDescriptionForNewTag(descriptionTag);
+  await tagDetailPage.clickExpandMetaData();
+  await tagDetailPage.enterMetaTitleForNewTag(metaTitleTag);
+  await tagDetailPage.enterMetaDescriptionForNewTag(metaDescriptionTag);
+  await tagDetailPage.clickSave();
+
+  let tagMetaTitleError = await tagDetailPage.tagMetaTitleError();
+  assert.strictEqual(tagMetaTitleError.trim(), 'Meta Title cannot be longer than 300 characters.');
+  
+  const correctMetaTitleTag = faker.datatype.string(300);
+  await homePage.goToTags();
+  await homePage.confirmLeaveCurrentPage();
+  await homePage.goToTags();
+  await tagsPage.goToCreateNewTag();
+  await tagDetailPage.enterTitleForNewTag(nameTag);
+  await tagDetailPage.enterDescriptionForNewTag(descriptionTag);
+  await tagDetailPage.clickExpandMetaData();
+  await tagDetailPage.enterMetaTitleForNewTag(correctMetaTitleTag);
+  await tagDetailPage.enterMetaDescriptionForNewTag(metaDescriptionTag);
+  await tagDetailPage.clickSave();
+  await homePage.goToTags();
+
+  let existsTag = await tagsPage.searchTagByName(nameTag);
+  assert.strictEqual(existsTag, true);
+});
+
 it('F01 - should create and publish post', async () => {
   test = "F01";
   const title = `${Date.now()}`;
