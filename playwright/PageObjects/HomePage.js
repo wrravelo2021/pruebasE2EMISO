34c943f867077840344dd16e591745fb5d940a36
William Ravelo M <drummerwilliam@gmail.com>
2022-11-01 15:22:19 -0500

    duplicate

--- playwright/PageObjects/HomePage.js
+++ playwright/PageObjects/HomePage.js
@@ -63,6 +63,10 @@ module.exports = class HomePage {
     return await this.page.click('.modal-footer > .gh-btn.gh-btn-red');
   }
 
+  async confirmLeaveCurrentPage() {
+    return await this.page.click('.modal-footer > .gh-btn.gh-btn-red');
+  }
+
   async please() {
 
   }
